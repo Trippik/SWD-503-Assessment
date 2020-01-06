a8d9f0df2e38a44d1d6cf91919250ff08de6ac04
Trippik <43576788+Trippik@users.noreply.github.com>
2020-01-06 15:13:12 +0000

    Create installer.bat

---
+++ interface/installer.bat
@@ -0,0 +1,8 @@
+@ECHO OFF 
+ECHO This script will install all necessary packages for the 'interface.py' program
+ECHO Please make sure this script is being run from the Scipts folder of your Python install directory
+PAUSE
+pip install psycopg2
+pip install pandas
+ECHO Installation complete
+PAUSE
