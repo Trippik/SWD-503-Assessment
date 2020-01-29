510b8f5fde99b387cbd48656a617c6ca2a15e6bd
Trippik <43576788+Trippik@users.noreply.github.com>
2020-01-29 13:41:39 +0000

    Update interface.py

--- interface/interface.py
+++ interface/interface.py
@@ -39,51 +39,57 @@ class simulator():
             return(grid)
 
     def __simulation(self, grid, columns, rows):
-        counterx = 0
-        countery = 0
-        while (countery != rows):
-            while (counterx != columns):
-                value = grid.iloc[int(countery), int(counterx)]
-                counterx = counterx + 1
-                print("Value: " + str(value))
-                if (value == 0):
-                    value1 = 0
-                    value2 = 0
-                    value3 = 0
-                    value4 = 0
-                    counter_y_plus = int(countery + 1)
-                    counter_y_minus = int(countery - 1)
-                    counter_x_plus = int(counterx + 1)
-                    counter_x_minus = int(counterx - 1)
-                    try:
-                       value1 = grid.iloc[int(counter_y_minus), int(counterx)]
-                    except:
-                        pass
-                    try: 
-                       value2 = grid.iloc[int(counter_y_plus), int(counterx)]
-                    except:
-                        pass
-                    try:
-                       value3 = grid.iloc[int(countery), int(counter_x_minus)]
-                    except:
-                        pass
-                    try:
-                       value4 = grid.iloc[int(countery), int(counter_x_plus)]
-                    except:
-                        pass
+        print("How many cycles do you wish to complete?")
+        cycles = int(input())
+        cycle_counter = 1
+        while (cycle_counter != cycles):
+            counterx = 0
+            countery = 0
+            while (countery != rows):
+                while (counterx != columns):
+                    value = grid.iloc[int(countery), int(counterx)]
+                    if (value == 0):
+                        value1 = 0
+                        value2 = 0
+                        value3 = 0
+                        value4 = 0
+                        counter_y_plus = int(countery + 1)
+                        counter_y_minus = int(countery - 1)
+                        counter_x_plus = int(counterx + 1)
+                        counter_x_minus = int(counterx - 1)
+                        try:
+                            value1 = grid.iloc[int(counter_y_minus), int(counterx)]
+                        except:
+                            pass
+                        try: 
+                            value2 = grid.iloc[int(counter_y_plus), int(counterx)]
+                        except:
+                            pass
+                        try:
+                            value3 = grid.iloc[int(countery), int(counter_x_minus)]
+                        except:
+                            pass
+                        try:
+                            value4 = grid.iloc[int(countery), int(counter_x_plus)]
+                        except:
+                            pass
                     
-                    combine = (value1 + value2 + value3 + value4)
-                    print("Sum Value: " + str(combine))
+                        combine = (value1 + value2 + value3 + value4)
                     
-                    if (combine > 2):
-                        print("x: " + str(counterx) + " y: " + str(countery) + " IT'S LIFE JIM")
-                    else:
-                        print("x: " + str(counterx) + " y: " + str(countery) + " :(")
+                        if (combine > 2):
+                            print("x: " + str(counterx) + " y: " + str(countery) + " IT'S LIFE JIM")
+                        else:
+                            print("x: " + str(counterx) + " y: " + str(countery) + " :(")
+                    counterx = counterx + 1
             else:
                 countery = countery + 1
                 counterx = 0
         else:
             print()
+            print()
+            print("Grid state at the end of cycle " + str(cycle_counter))
+            print()
+            print(grid)
 
 
     def run (self):
